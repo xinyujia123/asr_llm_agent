b2bbf2f04631113f7e0fa1d5d1a2d68825f3e505
xinyujia123 <xinyu_jia123@163.com>
2026-07-07 15:51:08 +0800

    update

--- agent_serve/smart_nursing_agent_0625.py
+++ agent_serve/smart_nursing_agent_0625.py
@@ -273,4 +273,4 @@ async def analyze_audio_endpoint(file: UploadFile = File(...)):
                 except: pass
 
 if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=8001)
+    uvicorn.run(app, host="0.0.0.0", port=8000)
